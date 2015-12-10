f5779057da8a217232f50959a9ccfda9297f6fe4
Zhoudunming Tu <tuzhoudunming@gmail.com>
2015-12-10 18:05:04 +0100

    add templates for various of cuts

--- pileUpFilter/python/pileUpFilter_cff.py
+++ pileUpFilter/python/pileUpFilter_cff.py
@@ -2,5 +2,19 @@ import FWCore.ParameterSet.Config as cms
 
 import pileUpCorrector.pileUpFilter.pileUpFilter_cfi
 
+pileUpFilter_default = pileUpCorrector.pileUpFilter.pileUpFilter_cfi.pileupVertexFilter.clone() 
+
 ### define your own filter ###
+
+pileUpFilter_baseCut = pileUpFilter_default.clone(
+## add cuts ##
+)
+
+pileUpFilter_baseCut_dz = pileUpFilter_default.clone(
+## add cuts ##
+)
+
+pileUpFilter_only1Vertex = pileUpFilter_default.clone(
+ 			   dzCutByNtrk = cms.vdouble( 999., 0.0, 0.0, 0.0, 0.0),
+                           dzTolerance = cms.double(9999.),)
 
