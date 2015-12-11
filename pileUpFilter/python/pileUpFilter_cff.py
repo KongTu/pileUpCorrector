542e61b92c85d3e967294f94de3fc8b91edcb05b
Zhoudunming Tu <tuzhoudunming@gmail.com>
2015-12-11 12:44:59 +0100

    update cff filters

--- pileUpFilter/python/pileUpFilter_cff.py
+++ pileUpFilter/python/pileUpFilter_cff.py
@@ -6,15 +6,63 @@ pileUpFilter_default = pileUpCorrector.pileUpFilter.pileUpFilter_cfi.pileupVerte
 
 ### define your own filter ###
 
-pileUpFilter_baseCut = pileUpFilter_default.clone(
-## add cuts ##
-)
+pileupFilter_baseCut_loose = pileUpFilter_default.clone(
+                                                           dzCutByNtrk = cms.vdouble(
+                                                                                    999.,999.,999.,4.8,1.9,
+                                                                                     1.2, 0.8, 0.8,0.8,0.6,
+                                                                                     0.5, 0.4, 0.4,0.3,0.2,
+                                                                                     0.2, 0.2, 0.2,0.2,0.2,
+                                                                                     0.1, 0.1, 0.1,0.1,0.1,
+                                                                                     0.1, 0.1, 0.1,0.1,0.1,
+                                                                                     0.1, 0.0
+                                                                                    ),  
+                                                           dxyVeto = cms.double(0.05)
+                                                       )
 
-pileUpFilter_baseCut_dz = pileUpFilter_default.clone(
-## add cuts ##
-)
+pileupFilter_baseCut_tight = pileUpFilter_default.clone(
+                                                           dzCutByNtrk = cms.vdouble(
+                                                                                    999.,999.,4.0,1.5,1.0,
+                                                                                     0.6, 0.5,0.4,0.3,0.3,
+                                                                                     0.3, 0.2,0.2,0.2,0.1,
+                                                                                     0.1, 0.1,0.1,0.1,0.1,
+                                                                                     0.1, 0.1,0.1,0.0
+                                                                                    ),
+                                                           dxyVeto = cms.double(0.05) 
+                                                       )
 
-pileUpFilter_only1Vertex = pileUpFilter_default.clone(
- 			   dzCutByNtrk = cms.vdouble( 999., 0.0, 0.0, 0.0, 0.0),
-                           dzTolerance = cms.double(9999.),)
+pileupFilter_baseCut_loose_dz1p0 = pileUpFilter_default.clone(
+                                                           dzCutByNtrk = cms.vdouble(
+                                                                                    999.,999.,999.,4.8,1.9,
+                                                                                     1.2, 0.8, 0.8,0.8,0.6,
+                                                                                     0.5, 0.4, 0.4,0.3,0.2,
+                                                                                     0.2, 0.2, 0.2,0.2,0.2,
+                                                                                     0.1, 0.1, 0.1,0.1,0.1,
+                                                                                     0.1, 0.1, 0.1,0.1,0.1,
+                                                                                     0.1, 0.0
+                                                                                    ),
+                                                           dxyVeto = cms.double(0.05),
+                                                           dzTolerance = cms.double(1.0)
+                                                       )
+
+pileupFilter_baseCut_tight_dz1p0 = pileUpFilter_default.clone(
+                                                           dzCutByNtrk = cms.vdouble(
+                                                                                    999.,999.,4.0,1.5,1.0,
+                                                                                     0.6, 0.5,0.4,0.3,0.3,
+                                                                                     0.3, 0.2,0.2,0.2,0.1,
+                                                                                     0.1, 0.1,0.1,0.1,0.1,
+                                                                                     0.1, 0.1,0.1,0.0
+                                                                                    ),
+                                                           dxyVeto = cms.double(0.05),
+                                                           dzTolerance = cms.double(1.0)
+                                                       )
+
+pileupFilter_vtx1 = pileUpFilter_default.clone(
+                                                           dzCutByNtrk = cms.vdouble(
+                                                                                     999.,0.0,0.0,0.0,0.0
+                                                                                    ),
+                                                           dzTolerance = cms.double(9999.0)
+
+                                                )                                      
+
+
 
